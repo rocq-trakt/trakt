4504decb08c5939e9b35a9b6346614f319504ff9
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-05-04 17:45:32 +0200

    chore(nix): Add support for Rocq 9.0 in tests

--- flake.nix
+++ flake.nix
@@ -192,9 +192,42 @@
 
           checks =
             let
-              # Waiting the version 3.23 of Dune for Rocq 9.0 builds
+              # NOTE: Waiting the version 3.23 of Dune for Rocq 9.0 builds
               # See: https://github.com/ocaml/dune/pull/14093
+              duneOverlay = final: prev: {
+                rocqPackages_9_0 = prev.rocqPackages_9_0.overrideScope (
+                  _: _: {
+                    dune = prev.ocamlPackages.dune_3.overrideAttrs rec {
+                      version = "3.23.0-alpha2";
+
+                      src = pkgs.fetchurl {
+                        url = "https://github.com/ocaml/dune/releases/download/3.23.0_alpha2/dune-3.23.0.alpha2.tbz";
+                        hash = "sha256-gYLRz/nYqn+JAs1QOTRyq9lPSTHbzHNznLEqBGoYTZM=";
+                      };
+                    };
+                  }
+                );
+              };
+
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = [
+                  duneOverlay
+                  self.overlays.default
+                ];
+              };
+
               combinaison = [
+                {
+                  rocq = "9.0";
+                  stdlib = "9.0";
+                  rocq-elpi = "3.3.0";
+                }
+                {
+                  rocq = "9.0";
+                  stdlib = "9.1";
+                  rocq-elpi = "3.3.0";
+                }
                 {
                   rocq = "9.1";
                   stdlib = "9.0";
