8db2f990f729629ff48231342cd7b1c6bf40dbae
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-07-07 23:43:28 +0200

    chore(nix): Split mkTrakt with mkTraktScope

--- nix/lib.nix
+++ nix/lib.nix
@@ -2,7 +2,7 @@
 
 with lib;
 {
-  mkTrakt =
+  mkTraktScope =
     pkgs:
     {
       rocq,
@@ -10,22 +10,34 @@ with lib;
       rocq-elpi,
       elpi ? null,
       trakt ? "dev",
-    }@attrs:
-    let
-      rocqPackages = (mkRocqPackages pkgs rocq).overrideScope (
-        _: prev:
-        {
-          stdlib = prev.stdlib.override { version = stdlib; };
-          rocq-elpi = prev.rocq-elpi.override { version = rocq-elpi; };
-          trakt = prev.trakt.override { version = trakt; };
-        }
-        // pkgs.lib.optionalAttrs (prev.rocq-core.version == "dev") {
-          zify = null;
-        }
-      );
-    in
+      ...
+    }:
+    (mkRocqPackages pkgs rocq).overrideScope (
+      _: prev:
+      {
+        stdlib = prev.stdlib.override { version = stdlib; };
+        rocq-elpi = prev.rocq-elpi.override {
+          version = rocq-elpi;
+          elpi-version = elpi;
+        };
+        trakt = prev.trakt.override { version = trakt; };
+      }
+      // pkgs.lib.optionalAttrs (prev.rocq-core.version == "dev") {
+        zify = null;
+      }
+    );
+
+  mkTrakt =
+    pkgs:
+    versions@{
+      rocq,
+      stdlib,
+      rocq-elpi,
+      trakt ? "dev",
+      ...
+    }:
     {
       name = "trakt-${mkFmt rocq}-${mkFmt stdlib}-${mkFmt rocq-elpi}-${mkFmt trakt}";
-      value = rocqPackages.trakt;
+      value = (mkTraktScope pkgs versions).trakt;
     };
 }
