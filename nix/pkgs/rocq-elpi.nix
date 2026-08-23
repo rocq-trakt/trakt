565387982fa4dd1fca2967932d9f9e07096cd009
Vincent Lafeychine <vincent.lafeychine@proton.me>
2026-08-23 15:06:25 +0200

    feat: Add release v3.5.0 of rocq-elpi

--- nix/pkgs/rocq-elpi.nix
+++ nix/pkgs/rocq-elpi.nix
@@ -21,6 +21,7 @@ overrideRocqDerivation (rec {
   pname = "rocq-elpi";
 
   defaultVersion = switch rocq-core.rocq-version [
+    (case (range "9.0" "9.2") "3.5.0")
     (case (range "9.0" "9.2") "3.4.0")
     (case (range "9.0" "9.2") "3.3.1")
     (case (range "9.0" "9.1") "3.2.0")
@@ -30,9 +31,9 @@ overrideRocqDerivation (rec {
     let
       rocq-elpi-version = if version == null then defaultVersion else version;
       elpi-version = switch rocq-elpi-version [
-        (case (versions.isEq "3.4.0") "3.7.1")
-        (case (versions.isEq "3.3.1") "3.6.2")
-        (case (versions.isEq "3.2.0") "3.4.5")
+        (case (versions.isGe "3.4.0") "3.7.1")
+        (case (versions.isGe "3.3.1") "3.6.2")
+        (case (versions.isGe "3.2.0") "3.4.5")
       ] "3.7.1";
     in
     [
@@ -42,6 +43,7 @@ overrideRocqDerivation (rec {
     ];
 
   release = {
+    "3.5.0".sha256 = "sha256-k6kbkFRngQmKUP0owiFOxJzdJVRte5IlZWG4Tpqd3YU=";
     "3.4.0".sha256 = "sha256-8x2Sa/+pUpXEqB+NdyfOyw6Yyzp6Q1k5LnhrjG/qJNM=";
     "3.3.1".sha256 = "sha256-/ehf2g/+riXOmfXs90Mx0PnKA0wJyombHK+rbVePjw0=";
     "3.2.0".sha256 = "sha256-FyYG/8lEyt1L/paMez8jYAnnUE+sxIp4Da5MztmwJ/c=";
